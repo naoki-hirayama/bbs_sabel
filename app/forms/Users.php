764cc8c0b6b6d20177b720fc92209d6c78bf3ebf
naoki-hirayama <bfmv.na.metalmaster820@gmail.com>
2019-05-30 16:36:40 +0900

    forms/Users.phpの配列の書き方統一

--- app/forms/Users.php
+++ app/forms/Users.php
@@ -2,21 +2,21 @@
 
 class Forms_Users extends Form_Model
 {
-    protected $displayNames = array(
+    protected $displayNames = [
         'name'             => '名前',
         'login_id'         => 'ログインID',
         'password'         => 'パスワード',
         'confirm_password' => 'パスワード(確認)',
         'picture'          => '画像',
         'comment'          => '一言コメント',
-    );
+    ];
 
-    protected $validators = array(
-        'name'                      => array('-strwidth', 'strlen(' . Users::MAX_NAME_LENGTH . ')'),
-        'login_id'                  => array('-strwidth', 'strlen(' . Users::MAX_LOGIN_ID_LENGTH . ')', 'alnum'), //第二引数でメソッドを指定
-        'password,confirm_password' => array('same'),
-        'confirm_password'          => array('required'),
-        'picture'                   => array('-strwidth', 'image("' . (Users::MAX_PICTURE_SIZE / 1024 / 1024) . 'M")'),
-        'comment'                      => array('-strwidth', 'strlen(' . Users::MAX_COMMENT_LENGTH . ')'),
-    );
+    protected $validators = [
+        'name'                      => ['-strwidth', 'strlen(' . Users::MAX_NAME_LENGTH . ')'],
+        'login_id'                  => ['-strwidth', 'strlen(' . Users::MAX_LOGIN_ID_LENGTH . ')', 'alnum'], 
+        'password,confirm_password' => ['same'],
+        'confirm_password'          => ['required'],
+        'picture'                   => ['-strwidth', 'image("' . (Users::MAX_PICTURE_SIZE / 1024 / 1024) . 'M")'],
+        'comment'                   => ['-strwidth', 'strlen(' . Users::MAX_COMMENT_LENGTH . ')'],
+    ];
 }
